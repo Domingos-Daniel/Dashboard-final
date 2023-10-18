a0f2c9bd9d9c0f2a3472b972d972cdbb65ac6cf0
Domingos Daniel <91840529+Domingos-Daniel@users.noreply.github.com>
2023-10-18 08:20:19 +0000

    Update 4 files

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -1,18 +1,53 @@
-import { Link } from "react-router-dom";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Input,
-  Checkbox,
-  Button,
-  Typography,
-} from "@material-tailwind/react";
+import React, { useState } from "react";
+import { Link} from "react-router-dom";
+import { Card, CardHeader, CardBody, CardFooter, Input, Checkbox, Button, Typography } from "@material-tailwind/react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Configurações do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA3pjjhao5HhdPfJRonDi6cQAwwPmM3KN0",
+  authDomain: "bni-atm.firebaseapp.com",
+  projectId: "bni-atm",
+  storageBucket: "bni-atm.appspot.com",
+  messagingSenderId: "1087416068939",
+  appId: "1:1087416068939:web:36f4337c62b9747de496ce",
+  measurementId: "G-FL067VKJ6W"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        toast.success("Login bem-sucedido! Redirecionando para a página inicial...");
+        setTimeout(() => {
+          window.location.href = "/home"; // Redirecionamento manual
+        }, 5000);
+        console.log("Login bem sucedido para:", user);
+        // Adicione lógica adicional aqui para lidar com o redirecionamento ou outras ações após o login bem-sucedido
+      })
+      .catch((error) => {
+        // Falha no login
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        toast.error(`Falha no login: ${errorMessage}`);
+        console.error("Login falhou com o erro:", errorMessage);
+        // Adicione lógica adicional aqui para lidar com a exibição de mensagens de erro para o usuário
+      });
+  };
+
   return (
     <>
+    
+    <ToastContainer />
       <img
         src="http://www.bfa.ao/images/image-share.jpg"
         className="absolute inset-0 z-0 h-full w-full object-cover"
@@ -30,17 +65,29 @@ export function SignIn() {
             </Typography>
           </CardHeader>
           <CardBody className="flex flex-col gap-4">
-            <Input type="email" label="Email" size="lg" />
-            <Input type="password" label="Password" size="lg" />
+            <Input
+              type="email"
+              label="Email"
+              size="lg"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              label="Password"
+              size="lg"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <div className="-ml-2.5">
               <Checkbox label="Lembrar-me" />
             </div>
           </CardBody>
           <CardFooter className="pt-0">
-            <Button variant="gradient" fullWidth>
+            <Button variant="gradient" fullWidth onClick={handleSignIn}>
               Entrar
             </Button>
-            <Typography variant="small" className="mt-6 flex justify-center hidden">
+            <Typography variant="small" className="mt-6 flex justify-center">
               Don't have an account?
               <Link to="/auth/sign-up">
                 <Typography
--- src/pages/auth/sign-up.jsx
+++ src/pages/auth/sign-up.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   Card,
@@ -9,15 +10,69 @@ import {
   Button,
   Typography,
 } from "@material-tailwind/react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
 
+
+// Configurações do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA3pjjhao5HhdPfJRonDi6cQAwwPmM3KN0",
+  authDomain: "bni-atm.firebaseapp.com",
+  projectId: "bni-atm",
+  storageBucket: "bni-atm.appspot.com",
+  messagingSenderId: "1087416068939",
+  appId: "1:1087416068939:web:36f4337c62b9747de496ce",
+  measurementId: "G-FL067VKJ6W"
+};
+
+// Inicialização do app e autenticação
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Componente de SignUp
 export function SignUp() {
+  // Estados para armazenar os valores dos campos
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+ // ... restante do código
+
+const handleSignUp = async (e) => {
+  e.preventDefault();
+
+  if (!email || !password) {
+    console.error("Email and password fields are required");
+    // Adicione lógica adicional aqui para notificar o usuário sobre os campos obrigatórios
+    return;
+  }
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    console.log("Sign up successful for:", user);
+    // Adicione lógica adicional aqui para lidar com o redirecionamento ou outras ações após o registro bem-sucedido
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error("Sign up failed with error:", errorMessage);
+    // Adicione lógica adicional aqui para lidar com a exibição de mensagens de erro para o usuário
+  }
+};
+
+// ... restante do código
+
+
+  // Retorno do componente
   return (
     <>
+      {/* Imagem de fundo e sobreposição */}
       <img
         src="http://www.bfa.ao/images/image-share.jpg"
         className="absolute inset-0 z-0 h-full w-full object-cover"
       />
       <div className="absolute inset-0 z-0 h-full w-full bg-black/50" />
+      {/* Conteúdo do formulário */}
       <div className="container mx-auto p-4">
         <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
           <CardHeader
@@ -30,17 +85,37 @@ export function SignUp() {
             </Typography>
           </CardHeader>
           <CardBody className="flex flex-col gap-4">
-            <Input label="Name" size="lg" />
-            <Input type="email" label="Email" size="lg" />
-            <Input type="password" label="Password" size="lg" />
+            {/* Campos de entrada para o nome, e-mail e senha */}
+            <Input
+              label="Name"
+              size="lg"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input
+              type="email"
+              label="Email"
+              size="lg"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              label="Password"
+              size="lg"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <div className="-ml-2.5">
               <Checkbox label="I agree the Terms and Conditions" />
             </div>
           </CardBody>
           <CardFooter className="pt-0">
-            <Button variant="gradient" fullWidth>
-              Sign Up
+            {/* Botão para registro */}
+            <Button variant="gradient" fullWidth onClick={handleSignUp}>
+              Cadastrar
             </Button>
+            {/* Link para a página de login */}
             <Typography variant="small" className="mt-6 flex justify-center">
               Already have an account?
               <Link to="/auth/sign-in">
@@ -61,4 +136,5 @@ export function SignUp() {
   );
 }
 
+// Exportação do componente SignUp
 export default SignUp;
