4494e9f2f0b21f4b63b9fcff1aee40ad39ac58cd
Domingos Daniel <91840529+Domingos-Daniel@users.noreply.github.com>
2023-10-23 14:23:06 +0000

    Update profile.jsx

--- src/pages/dashboard/profile.jsx
+++ src/pages/dashboard/profile.jsx
@@ -25,7 +25,7 @@ import NumeroFormulario from './NumeroFormulario';
 export function Profile() {
   return (
     <>
-      <div className="relative mt-8 h-72 w-full overflow-hidden rounded-xl bg-[url(http://www.bfa.ao/images/image-share.jpg)] bg-cover	bg-top">
+      <div className="relative mt-8 h-72 w-full overflow-hidden rounded-xl bg-[url(https://www.verangola.net/va/images/cms-image-000018011.jpg)] bg-cover	bg-top">
         <div className="absolute inset-0 h-full w-full bg-blue-500/50" />
       </div>
       <Card className="mx-3 -mt-16 mb-6 lg:mx-4">
