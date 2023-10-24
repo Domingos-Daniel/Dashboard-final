2b6e209a11ba0f4146acc4fa08d750e36d2c6201
Domingos Daniel <91840529+Domingos-Daniel@users.noreply.github.com>
2023-10-24 16:48:24 +0000

    Update ErrorLogs.jsx

--- src/pages/dashboard/ErrorLogs.jsx
+++ src/pages/dashboard/ErrorLogs.jsx
@@ -3,10 +3,7 @@ import Chart from "react-apexcharts";
 
 const ErrorLogs = ({ atm }) => {
   const [errorLogsData, setErrorLogsData] = useState({
-    errorFrequency: Array.from(
-      { length: 7 },
-      () => Math.floor(Math.random() * 9) + 1
-    ),
+    errorFrequency: Array.from({ length: 7 }, () => Math.floor(Math.random() * 9) + 1),
     possibleErrors: [
       "Falha ao dispensar dinheiro",
       "Falha na leitura de cartões",
@@ -16,7 +13,33 @@ const ErrorLogs = ({ atm }) => {
     ],
   });
 
+  const [selectedInterval, setSelectedInterval] = useState("daily");
+
   const { errorFrequency, possibleErrors } = errorLogsData;
+
+  const generateXAxisCategories = () => {
+    if (selectedInterval === "daily") {
+      return ["00:00", "06:00", "07:00", "13:00", "19:00", "23:59"];
+    } else if (selectedInterval === "weekly") {
+      return ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"];
+    } else if (selectedInterval === "monthly") {
+      return ["Sem 1", "Sem 2", "Sem 3", "Sem 4", "Sem 5"];
+    } else if (selectedInterval === "annual") {
+      return ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+    }
+  };
+
+  const generateSeriesData = () => {
+    if (selectedInterval === "daily") {
+      return Array.from({ length: 6 }, () => Math.floor(Math.random() * 9) + 1);
+    } else if (selectedInterval === "weekly") {
+      return Array.from({ length: 7 }, () => Math.floor(Math.random() * 9) + 1);
+    } else if (selectedInterval === "monthly") {
+      return Array.from({ length: 5 }, () => Math.floor(Math.random() * 9) + 1);
+    } else if (selectedInterval === "annual") {
+      return Array.from({ length: 12 }, () => Math.floor(Math.random() * 9) + 1);
+    }
+  };
 
   const getErrorSuggestion = () => {
     const maxErrorFrequency = Math.max(...errorFrequency);
@@ -35,6 +58,28 @@ const ErrorLogs = ({ atm }) => {
   return (
     <div className="overflow-y: auto h-4/6 w-4/6 rounded-lg bg-white p-4">
       <h2>Logs de Erros nos Últimos 7 Dias do {atm.name}</h2>
+      <select
+          value={selectedInterval}
+          onChange={(e) => setSelectedInterval(e.target.value)}
+          className="border border-gray-300 rounded-md p-1"
+        >
+          <option value="daily">Diário</option>
+          <option value="weekly">Semanal</option>
+          <option value="monthly">Mensal</option>
+          <option value="annual">Anual</option>
+        </select>
+      <div className="mt-4">
+        <p>
+          Nos últimos 7 dias, foram registrados os seguintes erros no ATM{" "}
+          {atm.name}:
+        </p>
+        <ul className="mt-2 list-inside list-disc">
+          {possibleErrors.map((error, index) => (
+            <li key={index}>{error}</li>
+          ))}
+        </ul>
+        <p className="mt-4 font-semibold">Sugestão: {getErrorSuggestion()}</p>
+      </div>
       <Chart
         options={{
           chart: {
@@ -54,7 +99,7 @@ const ErrorLogs = ({ atm }) => {
             },
           },
           xaxis: {
-            categories: ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"],
+            categories: generateXAxisCategories(),
           },
           plotOptions: { bar: { horizontal: false, barHeight: "50%" } },
           fill: { colors: ["#F87171"] },
@@ -62,25 +107,13 @@ const ErrorLogs = ({ atm }) => {
         series={[
           {
             name: "Frequência de Erros",
-            data: errorFrequency,
+            data: generateSeriesData(),
           },
         ]}
         type="bar"
         width={700}
         height={320}
       />
-      <div className="mt-4">
-        <p>
-          Nos últimos 7 dias, foram registrados os seguintes erros no ATM{" "}
-          {atm.name}:
-        </p>
-        <ul className="mt-2 list-inside list-disc">
-          {possibleErrors.map((error, index) => (
-            <li key={index}>{error}</li>
-          ))}
-        </ul>
-        <p className="mt-4 font-semibold">Sugestão: {getErrorSuggestion()}</p>
-      </div>
     </div>
   );
 };
