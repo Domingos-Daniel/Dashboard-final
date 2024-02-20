c9a17f94a63f217c74d0b52cba1c6b68c4468dd5
Domingos Daniel <91840529+Domingos-Daniel@users.noreply.github.com>
2024-02-20 15:44:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
+  build: {
+    rollupOptions: {
+      external: ['react-slick'],
+    },
+  },
 });
